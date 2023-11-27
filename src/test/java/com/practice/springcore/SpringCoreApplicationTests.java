4bd3fd80e907e1cde872c531a2230508ff42906d
grace <grace994084@gmail.com>
2023-11-27 22:24:02 +0900

    initial commit

---
+++ src/test/java/com/practice/springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
